0f84f20c2611d81d1c89fa853fd8b99fa5207f1d
scrapewalrus <88477231+scrapewalrus@users.noreply.github.com>
2021-08-19 14:44:38 +0300

    Add files via upload

---
+++ outputs.py
@@ -0,0 +1,17 @@
+
+import pandas as pd
+import os 	
+import json
+
+def output_json(all_data, keyword, loc):
+	if 'json' not in os.listdir():
+		os.mkdir('json')
+	with open(f'json/{keyword.replace("+", "-")}' + f'({loc}).json', 'w') as f:
+		json.dump(all_data, f, indent=4)
+
+
+def output_csv(all_data, keyword, loc):
+	if 'csv' not in os.listdir():
+		os.mkdir('csv')
+	df = pd.DataFrame(all_data)
+	df.to_csv(f'csv/{keyword.replace("+", "-")}' + f'({loc})', index=False)
